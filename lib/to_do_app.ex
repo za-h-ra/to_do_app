488c95f21191c584c7fbd67b07f0be0be0723e3d
Zahra Khan <zahra.khan1@icloud.com>
2022-12-02 17:50:49 -0500

    parse string to integer

--- lib/to_do_app.ex
+++ lib/to_do_app.ex
@@ -1,9 +1,9 @@
 defmodule ToDoApp do
   def create_tasks do
+    # Pattern Matching
     number_of_tasks = IO.gets("How many tasks would you want to complete today?")
     # Convert String into Integer
     {number_of_tasks, _q} = Integer.parse(number_of_tasks)
-
+    number_of_tasks
   end
-
 end

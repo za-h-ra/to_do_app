c8c750f79ab0099c924ee824ae6f3f94be155d97
Zahra Khan <zahra.khan1@icloud.com>
2022-12-02 18:01:25 -0500

    [REFACTOR]

--- lib/to_do_app.ex
+++ lib/to_do_app.ex
@@ -1,9 +1,7 @@
 defmodule ToDoApp do
   def create_tasks do
-    # Pattern Matching
-    number_of_tasks = IO.gets("How many tasks would you want to complete today?")
-    # Convert String into Integer
-    {number_of_tasks, _q} = Integer.parse(number_of_tasks)
-    number_of_tasks
+    # Convert str to integer
+    {num_of_tasks, _} = IO.gets("How many tasks would you want to complete today? ") |> Integer.parse()
+    num_of_tasks
   end
 end

3624f0730c1af7a2b0f9fea3ce7610d7c76890e4
Zahra Khan <zahra.khan1@icloud.com>
2022-12-02 18:04:52 -0500

    create test task list

--- lib/to_do_app.ex
+++ lib/to_do_app.ex
@@ -1,7 +1,14 @@
 defmodule ToDoApp do
   def create_tasks do
-    # Convert str to integer
-    {num_of_tasks, _} = IO.gets("How many tasks would you want to complete today? ") |> Integer.parse()
+    # Get # of tasks
+    # Convert String to Integer
+    {num_of_tasks, _} =
+      IO.gets("How many tasks would you want to complete today? ") |> Integer.parse()
+
     num_of_tasks
+  end
+
+  def test_task_list do
+    ["meditate", "exercise", "do laundry"]
   end
 end
